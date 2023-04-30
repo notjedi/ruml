ee46679c3d0b96de9d27eea9a42f0cbffa600526
Krithic Kumar <krithickumarub@protonmail.com>
2023-05-01 01:14:23 +0530

    feat: initial implementation of Tensor class

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -9,6 +9,7 @@ impl Shape {
         // Compute default array strides
         // Shape (a, b, c) => Give strides (b * c, c, 1)
         // Right now, we only support row major stride by default
+        // TODO: return error if any of the elems in dim is 0
         let mut strides = vec![1; dim.len()].into_boxed_slice();
         let stride_it = strides.iter_mut().rev();
         let mut cum_prod = 1;
@@ -23,6 +24,10 @@ impl Shape {
     pub fn ndim(&self) -> usize {
         self.dim.len()
     }
+
+    pub fn numel(&self) -> usize {
+        self.dim.iter().fold(1, |acc, &i| acc * i)
+    }
 }
 
 impl From<Vec<usize>> for Shape {
@@ -34,8 +39,17 @@ impl From<Vec<usize>> for Shape {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Tensor<T> {
-    data: T,
+    // TODO: should i take a pointer to the data?
+    data: Box<[T]>,
     dim: Shape,
+}
+
+impl<T> Tensor<T> {
+    pub fn from_shape(dim: Shape, data: Box<[T]>) -> Self {
+        // TODO: return error if len(data) != shape.numel()
+        assert_eq!(data.len(), dim.numel());
+        Self { data, dim }
+    }
 }
 
 #[cfg(test)]
@@ -46,7 +60,18 @@ mod tests {
     #[test]
     fn test_shape() {
         let shape: Shape = vec![2, 2, 5].into();
+        assert_eq!(shape.numel(), 2 * 2 * 5);
         assert_eq!(shape.dim.borrow(), [2, 2, 5]);
         assert_eq!(shape.strides.borrow(), [10, 5, 1]);
+    }
+
+    #[test]
+    fn test_tensor() {
+        let shape: Shape = vec![2, 2, 2].into();
+        let data = Box::new([1; 2 * 2 * 2]);
+        let tensor: Tensor<i32> = Tensor::from_shape(shape, data);
+
+        // TODO: assert that this is actually equal to a 3d tensor
+        assert_eq!(tensor.data.borrow(), [1, 1, 1, 1, 1, 1, 1, 1]);
     }
 }
